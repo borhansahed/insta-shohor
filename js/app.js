ed34319070145b62dde54d9e08e6db57ed52910e
wahi90 <mdshahed1098@gmail.com>
2022-03-11 16:01:30 +0600

    show only reported posts

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const getReportedPosts = () => {
 };
 
 const isLiked = (id) => {
-    return likedPostsId?.length && !!likedPostsId.includes(id);
+    return likedPostsId.length && likedPostsId.includes(id);
 };
 
 const addToLiked = (id) => {
@@ -51,7 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post)
+  
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -153,7 +153,8 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
